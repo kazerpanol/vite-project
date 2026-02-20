ac5f1c222ef2338362d9eb467e136d865a71af07
Kazer Panol <kazer.panol8@gmail.com>
2026-02-20 09:53:06 +0800

    add users

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import data from "./data";
-import List from "./List";
 
 function App() {
-  const [people, setPeople] = useState(data);
-  const handleClear = () => {
-    setPeople([]);
-  };
+  const url = "https://api.github.com/users";
+
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>Employee</h1>
-      {<List people={people} />}
-      {console.log(people)}
-      <button onClick={handleClear}>Clear All</button>
-    </div>
+    <section>
+      {users?.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+
+        return (
+          <li className="card" key={id}>
+            <img className="avatar" src={avatar_url} />
+            <div>
+              <h5>{login}</h5>
+              <a href={html_url}>Profile</a>
+            </div>
+          </li>
+        );
+      })}
+    </section>
   );
 }
 
